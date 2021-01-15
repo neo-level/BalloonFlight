6e169b8a4f640e4a659396534016db6bdc94d7ae
frederik desmet <info@neo-level.com>
2021-01-15 11:50:49 +0100

    Added a box collider to find the exact size of the object, divided it by half to make the transition smoother, removed hardcoded elements.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private float _repeatWidth;
     private Vector3 _startPosition;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get the x width of the box collider from the object and divide it by half.
+        _repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        
         // Set the startposition to the original position of the background.
         _startPosition = transform.position;
     }
@@ -17,9 +22,9 @@ public class RepeatBackground : MonoBehaviour
     void Update()
     {
         // Reset position if the background removes a certain distance.
-        if (transform.position.x < -50)
+        if (transform.position.x < _repeatWidth)
         {
-            // Set the position of the backround back to its original values.
+            // Set the position of the background back to its original values.
             transform.position = _startPosition;
         }
     }
