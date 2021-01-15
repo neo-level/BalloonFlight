02d700713a25e0a7532c3ee1dfbfe7c2f7c02c04
frederik desmet <info@neo-level.com>
2021-01-15 11:06:28 +0100

    set variables to hold the game object and its position for spawning, in the start method create an instance of the obstacle.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePrefab;
+
+    private Vector3 _spawnPosition = new Vector3(25, 0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
+        // Create instance of the obstacle object and apply its location and rotation.
+        Instantiate(obstaclePrefab, _spawnPosition, obstaclePrefab.transform.rotation);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
