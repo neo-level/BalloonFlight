a87389e0ea23dec19d426f3e7e2f500a3746a539
frederik desmet <info@neo-level.com>
2021-01-15 16:51:38 +0100

    bonud: Make the balloon appear to bounce off of the ground, preventing it from leaving the bottom of the screen. There should be a sound effect when this happens, too!
    Created a new script to set boundaries. used the background y size as a reference for the max height the balloon could go.

--- Assets/Challenge 3/Scripts/BoundaryX.cs
+++ Assets/Challenge 3/Scripts/BoundaryX.cs
@@ -2,32 +2,41 @@
 using System.Collections.Generic;
 using TMPro.SpriteAssetUtilities;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BoundaryX : MonoBehaviour
 {
     private float _topBoundary;
-    private float _bottomBoundary = -10f;
+    public bool isLowEnough = true;
+    private float _bounceForce = 10.0f;
+
+    private Rigidbody _playerRigidbody;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        _playerRigidbody = GetComponent<Rigidbody>();
+
+        // Set top boundary based on the background y size.
         _topBoundary = GameObject.Find("Background").GetComponent<BoxCollider>().size.y;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y > _topBoundary )
+        if (transform.position.y > _topBoundary)
         {
+            isLowEnough = false;
             SetBoundary(_topBoundary);
+            _playerRigidbody.AddForce(Vector3.down * _bounceForce, ForceMode.Impulse);
         }
-        else if (transform.position.y < _bottomBoundary)
+        else if (transform.position.y < _topBoundary)
         {
-            SetBoundary(_bottomBoundary);
+            isLowEnough = true;
         }
     }
-    
+
     private void SetBoundary(float threshold)
     {
         transform.position = new Vector3(x: transform.position.x, y: threshold, z: transform.position.z);
--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -1,46 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
 
-    public float floatForce;
-    private float gravityModifier = 1.5f;
+    private float _floatForce = 20.0f;
+    private float _gravityModifier = 1.5f;
+    
     private Rigidbody _playerRb;
-
+    private BoundaryX _boundaryXScript;
+    
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
 
     private AudioSource _playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
-
+    // Added public audio clip as a placeholder for the audio file.
+    public AudioClip boingSound;
+    private float _bounceForce = 10.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        Physics.gravity *= gravityModifier;
+        Physics.gravity *= _gravityModifier;
         _playerAudio = GetComponent<AudioSource>();
-        
+
         // Get the rigidbody component.
         _playerRb = GetComponent<Rigidbody>();
 
         // Apply a small upward force at the start of the game
         _playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
         
+        // Get the players script component.
+        _boundaryXScript = GameObject.Find("Player").GetComponent<BoundaryX>();
         
-
     }
 
     // Update is called once per frame
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver && _boundaryXScript.isLowEnough)
         {
-            _playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+            _playerRb.AddForce(Vector3.up * _floatForce, ForceMode.Impulse);
         }
     }
 
@@ -54,7 +61,7 @@ public class PlayerControllerX : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
-        } 
+        }
 
         // if player collides with money, fireworks
         else if (other.gameObject.CompareTag("Money"))
@@ -62,9 +69,15 @@ public class PlayerControllerX : MonoBehaviour
             fireworksParticle.Play();
             _playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            // Addition of the "boing" sound when hitting the ground.
+            _playerAudio.PlayOneShot(boingSound, volumeScale: 5.0f);
+            
+            // When hitting the ground bounce back.
+            _playerRb.AddForce(Vector3.up * _bounceForce, ForceMode.Impulse);
 
         }
-
     }
-
 }
