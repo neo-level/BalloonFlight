44fe1cc957d91b0af931a91a564c6bb0019befcc
frederik desmet <info@neo-level.com>
2021-01-15 11:46:59 +0100

    extracted the start position of the background. reset its position back to its original place once it passes a certain distance.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 _startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set the startposition to the original position of the background.
+        _startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Reset position if the background removes a certain distance.
+        if (transform.position.x < -50)
+        {
+            // Set the position of the backround back to its original values.
+            transform.position = _startPosition;
+        }
     }
 }
