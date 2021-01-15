594a7f4e901dfa9bab8a619a16c8e1a3600857f8
frederik desmet <info@neo-level.com>
2021-01-15 15:41:15 +0100

    Fixed the bug: The balloon can float way too high.
    Created a new script to set boundaries. used the background y size as a reference for the max height the balloon could go.

---
+++ Assets/Challenge 3/Scripts/BoundaryX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.SpriteAssetUtilities;
+using UnityEngine;
+
+public class BoundaryX : MonoBehaviour
+{
+    private float _topBoundary;
+    private float _bottomBoundary = -10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _topBoundary = GameObject.Find("Background").GetComponent<BoxCollider>().size.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > _topBoundary )
+        {
+            SetBoundary(_topBoundary);
+        }
+        else if (transform.position.y < _bottomBoundary)
+        {
+            SetBoundary(_bottomBoundary);
+        }
+    }
+    
+    private void SetBoundary(float threshold)
+    {
+        transform.position = new Vector3(x: transform.position.x, y: threshold, z: transform.position.z);
+    }
+}
