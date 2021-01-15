aa2b3e1c2681bf58214847b78ef7be22dda14258
frederik desmet <info@neo-level.com>
2021-01-15 11:14:32 +0100

    altered script so that the obstacles are spawned on a time interval using the InvokeRepeating method. Moved the instantiate into a method that is used in the interval method.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.XR.WSA.Input;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
 
+    // Fixed spawning postion for the obstacles.
     private Vector3 _spawnPosition = new Vector3(25, 0, 0);
 
+    // Set the time when to start spawning and at what rate.
+    private float _startDelay = 2.0f;
+    private float _repeatRate = 2.0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        // Create instance of the obstacle object and apply its location and rotation.
-        Instantiate(obstaclePrefab, _spawnPosition, obstaclePrefab.transform.rotation);
-        
+        // Spawn obstacles on an interval base.
+        InvokeRepeating(methodName: nameof(SpawnObstacle),time: _startDelay,repeatRate: _repeatRate);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+    }
+
+    // Spawns an obstacle everytime it's called.
+    private void SpawnObstacle()
+    {
+        // Create instance of the obstacle object and apply its location and rotation.
+        Instantiate(obstaclePrefab, _spawnPosition, obstaclePrefab.transform.rotation);
     }
 }
