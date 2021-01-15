2570ab402b171beacad3434676a8ddd73727b127
frederik desmet <info@neo-level.com>
2021-01-15 10:19:09 +0100

    add jump when space is pressed.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,13 +11,17 @@ public class PlayerController : MonoBehaviour
     {
         // Gets the rigidbody component from the object.
         playerRigidbody = GetComponent<Rigidbody>();
-        // Add physics to the player, letting it jump upwards.
-        playerRigidbody.AddForce(Vector3.up * 500);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Adds physics to the player allowing an upwards jump.
+            // Immediately applies force. using the impulse mode.
+            playerRigidbody.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
