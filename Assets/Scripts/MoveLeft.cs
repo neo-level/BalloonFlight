ee49b90358bef6bc71b6dc1808608c140c628f62
frederik desmet <info@neo-level.com>
2021-01-15 12:06:46 +0100

    Stop object from moving once it's game over. adding a reference to the playerController script. get the script component and validate the boolean gameOver.

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float _speed = 30.0f;
+    private PlayerController _playerControllerScript;
 
     // Start is called before the first frame update
     private void Start()
     {
+        // Get the players script component.
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // Move the object to the left on a time base.
-        transform.Translate(Vector3.left * (Time.deltaTime * _speed));
+        // If the boolean game over is false continue to make the objects move.
+        if (!_playerControllerScript.gameOver)
+        {
+            // Move the object to the left on a time base.
+            transform.Translate(Vector3.left * (Time.deltaTime * _speed));
+        }
     }
 }
