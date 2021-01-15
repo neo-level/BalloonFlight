1691182010a4f072a12d31687be308b2a33c3fcc
frederik desmet <info@neo-level.com>
2021-01-15 10:08:43 +0100

    Addition of interaction with the rigidbody component. Force applied in an upwards motion.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Gets the rigidbody component from the object.
+        playerRigidbody = GetComponent<Rigidbody>();
+        // Add physics to the player, letting it jump upwards.
+        playerRigidbody.AddForce(Vector3.up * 500);
     }
 
     // Update is called once per frame
