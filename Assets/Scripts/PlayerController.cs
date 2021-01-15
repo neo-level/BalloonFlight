75ae8b259921ff5b494632df289a794e08cbe175
frederik desmet <info@neo-level.com>
2021-01-15 10:33:47 +0100

    comments added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,17 +21,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if space is pressed and if the player is on the ground.
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             // Adds physics to the player allowing an upwards jump.
             // Immediately applies force. using the impulse mode.
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            // The player is in the air so set boolean to false.
             isOnGround = false;
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        // When the player collides with the ground set back to true.
         isOnGround = true;
     }
 }
