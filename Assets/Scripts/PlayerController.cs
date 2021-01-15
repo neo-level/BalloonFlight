e26bfc5b760e478689efeacd05989ee4add2074d
frederik desmet <info@neo-level.com>
2021-01-15 10:32:01 +0100

    addition of logic to prevent jump spam. boolean to check if char is on the ground with the oncollisionenter method.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,25 +8,30 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRigidbody;
     public float jumpForce;
     public float gravityModifier;
-    
+    public bool isOnGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
         // Gets the rigidbody component from the object.
         playerRigidbody = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-        
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             // Adds physics to the player allowing an upwards jump.
             // Immediately applies force. using the impulse mode.
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isOnGround = true;
     }
 }
