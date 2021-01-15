38aa2499c0ead7ac34b23575a36464a386577905
frederik desmet <info@neo-level.com>
2021-01-15 10:46:19 +0100

    Creation of move left script. to allow the objects attached move left in the scene. this apples to background and object. Creating a moving world.

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float _speed = 30.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Move the object to the left on a time base.
+        transform.Translate(Vector3.left * (Time.deltaTime * _speed));
+    }
+}
